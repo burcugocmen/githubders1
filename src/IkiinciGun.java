e30458d78ab68554a0e232cd100a96ef939e6ea2
burcugocmen <Burcubaybars127@gmail.com>
2023-09-28 20:30:30 +0300

    initial commit

---
+++ src/IkiinciGun.java
@@ -0,0 +1,6 @@
+public class IkiinciGun {
+    public static void main(String[] args) {
+        System.out.println("Yeni bir islem yapiyırom");
+    }
+
+}
--- src/İlkGitHesabi.java
+++ src/İlkGitHesabi.java
@@ -1,5 +1,6 @@
 public class İlkGitHesabi {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+        System.out.println("Push ");
     }
 }
